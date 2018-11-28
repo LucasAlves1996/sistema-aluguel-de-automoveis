74e4387128969c9ed667e2f924b2d3d493c2e4c1
Lucas Costa Alves <lucas.alves263@gmail.com>
2018-11-28 13:16:21 +0000

    Update veiculo.php

--- veiculo.php
+++ veiculo.php
@@ -2,6 +2,11 @@
 
 session_start();
 
+if(!isset($_SESSION['user'])){
+	$_SESSION['acessoSemLogin'] = "Para acessar esta página você deve fazer login!";
+	header('Location: index.php');
+}
+
 include ("html/layout-default/top.php");
 
 require ("classes/automovel.php");
